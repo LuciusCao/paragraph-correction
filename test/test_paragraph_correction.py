d9498e7a8c56e8884e2f2f48a6fa2854f4851b49
LuciusCao <lucius.cao@gmail.com>
2018-06-13 07:48:57 +0800

    add test

---
+++ test/test_paragraph_correction.py
@@ -0,0 +1,25 @@
+from src.paragraph_correction import ParagraphCorrection
+
+import pytest
+
+
+@pytest.mark.parametrize(
+    'wrong_paragraph, correct_paragraph, full_diffs, diff, answer_key', [
+        ('I am a boy', 'I was a girl',
+         [(0, 'I'), (-1, 'am'),
+          (1, 'was'), (0, 'a'),
+          (-1, 'boy'), (1, 'girl')],
+         [(-1, 'am'), (1, 'was'),
+          (-1, 'boy'), (1, 'girl')],
+         {0: [(-1, 'am'), (1, 'was')],
+          1: [(-1, 'boy'), (1, 'girl')]},
+         ),
+    ])
+def test_initialization(wrong_paragraph, correct_paragraph, full_diffs,
+                        diff, answer_key):
+    para_crct = ParagraphCorrection(wrong_paragraph, correct_paragraph)
+    assert para_crct.wrong_paragraph == wrong_paragraph
+    assert para_crct.correct_paragraph == correct_paragraph
+    assert para_crct.full_diffs == full_diffs
+    assert para_crct.diff == diff
+    assert para_crct.answer_key == answer_key
