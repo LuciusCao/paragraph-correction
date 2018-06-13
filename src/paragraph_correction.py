6178e282a668c1fccbdcee3369d283d7e4ee5438
Lucius Cao <lucius.cao@gmail.com>
2018-06-13 13:20:34 +0800

    change method to support user input

--- src/paragraph_correction.py
+++ src/paragraph_correction.py
@@ -7,12 +7,17 @@ class ParagraphCorrection:
         self.correct_paragraph = correct_paragraph
         self.full_diffs = self.__diff_wordmode()
         self.diff = self.__diff_only()
-        self.answer_key = self.__get_answer_key_from_diff()
+        self.answer_key = self.__get_answer_from_diff()
 
-    def __diff_wordmode(self):
+    def __diff_wordmode(self, paragraph_one=None, paragraph_two=None):
+        if paragraph_one is None:
+            paragraph_one = self.wrong_paragraph
+
+        if paragraph_two is None:
+            paragraph_two = self.correct_paragraph
+
         d = dmp()
-        words = d.diff_linesToWords(self.wrong_paragraph,
-                                    self.correct_paragraph)
+        words = d.diff_linesToWords(paragraph_one, paragraph_two)
         word_text_one = words[0]
         word_text_two = words[1]
         word_array = words[2]
@@ -21,16 +26,21 @@ class ParagraphCorrection:
         full_diffs = [(c, s.strip()) for c, s in full_diffs]
         return full_diffs
 
-    def __diff_only(self):
-        return [diff for diff in self.full_diffs if diff[0] != 0]
+    def __diff_only(self, full_diffs=None):
+        if full_diffs is None:
+            full_diffs = self.full_diffs
 
-    def __get_answer_key_from_diff(self):
-        answer_key = {}
+        return [diff for diff in full_diffs if diff[0] != 0]
+
+    def __get_answer_from_diff(self, full_diffs=None):
+        if full_diffs is None:
+            full_diffs = self.full_diffs
+        answer = {}
         cur_answer = 0
         started = False
         start_index = end_index = None
 
-        for i, diff in enumerate(self.full_diffs):
+        for i, diff in enumerate(full_diffs):
             if not started and diff[0] != 0:
                 start_index = i
                 started = True
@@ -39,12 +49,12 @@ class ParagraphCorrection:
                 started = False
 
             start_w_end = start_index and end_index
-            start_wo_end = i == len(self.full_diffs) - 1 and started
+            start_wo_end = i == len(full_diffs) - 1 and started
 
             if start_w_end or start_wo_end:
                 s = slice(start_index, end_index)
-                answer_key[cur_answer] = self.full_diffs[s]
+                answer[cur_answer] = full_diffs[s]
                 cur_answer += 1
                 start_index = end_index = None
 
-        return answer_key
+        return answer
